fdf328bc2e68045fa3bd25228f1036fd974b1361
20222014040009 <a.maycon@escolar.ifrn.edu.br>
2024-08-17 16:17:10 -0300

    adapta jogo para UG de 4px

--- Pitfall.asm
+++ Pitfall.asm
@@ -1,39 +1,39 @@
 .text
 .main:
 	lui $s0, 0x1001           # início do endereço de memória
-	addi $s1, $zero, 32       # L: quantidade de UG na linha
+	addi $s1, $zero, 128      # L: quantidade de UG na linha
 	
 	add $t0, $zero, $zero     # iterador
 	
 	addi $t1, $zero, 0x345C1C # cor da Copa
-	addi $a1, $zero, 96       # UG limite
+	addi $a1, $zero, 1536     # UG limite
 	jal gerarCenario
 	addi $t1, $zero, 0x6C9850 # cor da Floresta
-	addi $a1, $zero, 256      # UG limite
+	addi $a1, $zero, 4096     # UG limite
 	jal gerarCenario
 	addi $t1, $zero, 0xB8B840 # cor da Faixa de Terra 1
-	addi $a1, $zero, 320      # UG limite
+	addi $a1, $zero, 5120     # UG limite
 	jal gerarCenario
 	addi $t1, $zero, 0x848424 # cor da Faixa de Terra 2
-	addi $a1, $zero, 384      # UG limite
+	addi $a1, $zero, 6144     # UG limite
 	jal gerarCenario
 	
 	lui $s0, 0x1001           # início do endereço de memória
-	addi $t2, $zero, 3        # l: linha
+	addi $t2, $zero, 12       # l: linha
 	addi $t1, $zero, 0x444400 # cor do Tronco
 	
-	addi $a1, $zero, 6  # c: coluna
+	addi $a1, $zero, 24  # c: coluna
 	jal gerarTronco
-	addi $a1, $zero, 13 # c: coluna
+	addi $a1, $zero, 52  # c: coluna
 	jal gerarTronco
-	addi $a1, $zero, 18 # c: coluna
+	addi $a1, $zero, 72  # c: coluna
 	jal gerarTronco
-	addi $a1, $zero, 25 # c: coluna
+	addi $a1, $zero, 100 # c: coluna
 	jal gerarTronco
 	
-	lui $s0, 0x1001	   # início do endereço de memória
-	addi $t2, $zero, 6 # l: linha
-	addi $a1, $zero, 2 # c: coluna
+	lui $s0, 0x1001	    # início do endereço de memória
+	addi $t2, $zero, 24 # l: linha
+	addi $a1, $zero, 8  # c: coluna
 	jal gerarPersonagem
 	
 	addi $2, $zero, 10 # >>> ENDPROGRAM <<<
@@ -59,12 +59,15 @@
 			sll $t6, $t6, 2
 			add $t6, $t6, $s0
 			
-			for_gT: beq $t5, 5, EXIT_gerarCenario # pinta 5 UG de Tronco
-				
+			for_gT: beq $t5, 20, EXIT_gerarCenario # pinta 20 UG de Tronco
+				# espessura do tronco: 4 UG
 				sw $t1, 0($t6)
+				sw $t1, 4($t6)
+				sw $t1, 8($t6)
+				sw $t1, 12($t6)
 			
 				addi $t5, $t5, 1 # incremento
-				addi $t6, $t6, 128
+				addi $t6, $t6, 512
 				
 				j for_gT
 			
@@ -80,20 +83,67 @@
 		sll $t6, $t6, 2
 		add $t6, $t6, $s0
 		
+		HAIR:
+			addi $t1, $zero, 0x646410 # cor do Cabelo
+			sw $t1, 0($t6)
+			sw $t1, 4($t6)
+			sw $t1, 8($t6)
 		HEAD:
 			addi $t1, $zero, 0xE4706F # cor da Cabeça
-			sw $t1, 0($t6)
+			add $t0, $zero, $zero     # reseta iterador
+			
+			for_HEAD: beq $t0, 2, EXIT_for_HEAD
+				  
+				  addi $t6, $t6, 512
+				  sw $t1, 0($t6)
+				  sw $t1, 4($t6)
+				  sw $t1, 8($t6)
+				  
+				  addi $t0, $t0, 1 # incremento
+				  j for_HEAD
+				  
+			EXIT_for_HEAD: 
+				# Pescoço
+				addi $t6, $t6, 512
+				sw $t1, 0($t6)
+				
 		SHIRT:
 			addi $t1, $zero, 0x5BBB5C # cor da Camisa
-			addi $t6, $t6, 128
-			sw $t1, 0($t6)
-			addi $t6, $t6, 4
-			sw $t1, 0($t6)
+			add $t0, $zero, $zero     # reseta iterador
+			
+			for_SHIRT: beq $t0, 5, PANTS
+				   
+				   addi $t6, $t6, 512
+				   sw $t1, 0($t6)
+				   sw $t1, 4($t6)
+				   sw $t1, 8($t6)
+				   
+				   addi $t0, $t0, 1 # incremento
+				   j for_SHIRT
+			
 		PANTS:
 			addi $t1, $zero, 0x356018 # cor da Calça
-			addi $t6, $t6, 124
-			sw $t1, 0($t6)
-			addi $t6, $t6, 128
-			sw $t1, 0($t6)
+			add $t0, $zero, $zero     # reseta iterador
+			
+			for_PANTS: beq $t0, 5, EXIT_for_PANTS
+				
+				   addi $t6, $t6, 512
+				   sw $t1, 0($t6)
+				   sw $t1, 4($t6)
+				   sw $t1, 8($t6)
+				
+				   addi $t0, $t0, 1 # incremento
+				   j for_PANTS
+				
+			EXIT_for_PANTS:
+				# Pé esquerdo
+				sw $t1, 12($t6)
+				# Pé direito
+				addi $t6, $t6, 512
+				sw $t1, 0($t6)
+				addi $t6, $t6, 512
+				sw $t1, 0($t6)
+				sw $t1, 4($t6)
+				sw $t1, 8($t6)
 		
 	EXIT_gerarPersonagem: jr $31
